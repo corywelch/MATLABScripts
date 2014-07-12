6702569205b261903b95c7e0c44053e5763181e7
Cory Welch <cory.welch77@gmail.com>
2014-07-12 18:01:45 -0400

    Update SYDE112run.m
    Reworked code to not need f file at all

--- SYDE112run.m
+++ SYDE112run.m
@@ -1,2 +1,24 @@
-fprintf('Run NumIntegrate\n');
-numintegrate(1,2,10,'simpson')
+clc, clear,  format compact
+
+%enter bounds
+lowerBound = 1;
+upperBound = 2;
+
+%enter n value
+n = 100;
+
+%no need the change these
+length = (upperBound - lowerBound)/n;
+x = lowerBound:length/100:upperBound;
+
+%enter function here
+fx = sqrt(1 + x.^(-4));
+
+%enter type of integration estimation
+type = 'simpson';
+
+answer = numintegratenew(lowerBound,upperBound,n,type,fx);
+
+fprintf('The Integral of f(x) as defined between the bounds %i and %i',lowerBound,upperBound);
+fprintf('\nWith %i intervals',n);
+fprintf('\nIs %f',answer);
