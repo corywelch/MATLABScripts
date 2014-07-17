ec32998b489529c53735d16c9481d914a1fd7120
Cory Welch <cory.welch77@gmail.com>
2014-07-17 09:47:26 -0400

    Update SYDE112_run.m
    Fixed

--- SYDE112_run.m
+++ SYDE112_run.m
@@ -5,19 +5,20 @@ lowerBound = 1;
 upperBound = 2;
 
 %enter n value
-n = 100;
+n = 10;
 
 %no need the change these
 length = (upperBound - lowerBound)/n;
-x = lowerBound:length/100:upperBound;
+x = lowerBound:length:upperBound;
 
 %enter function here
-fx = sqrt(1 + x.^(-4));
+fx =x.^5;
 
 %enter type of integration estimation
 type = 'simpson';
 
 answer = numintegratenew(lowerBound,upperBound,n,type,fx);
+
 
 fprintf('The Integral of f(x) as defined between the bounds %i and %i',lowerBound,upperBound);
 fprintf('\nWith %i intervals',n);
